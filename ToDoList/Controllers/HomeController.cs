e64d362e70c869cc7f0c4b9f3fa2cc22aa62c540
Aaron Brown <worldofaaron@hotmail.com>
2023-10-16 19:50:13 -0700

    update splash page w/ m2m

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly ToDoListContext _db;
+      public HomeController(ToDoListContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        Category[] cats = _db.Categories.ToArray();
+        Item[] items = _db.Items.ToArray();
+        Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+        model.Add("categories", cats);
+        model.Add("items", items);
+        return View(model);
       }
 
     }
